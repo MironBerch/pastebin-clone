6f8e716ebbd36ed084d7075d45af2a571379159d
MironBerch <mironberchetov@gmail.com>
2023-11-03 02:53:20 +0300

    Update celery config

---
+++ images/backend/src/core/celery.py
@@ -0,0 +1,20 @@
+from celery import Celery
+
+from core.config import settings
+
+redis = settings.redis
+
+celery_app = Celery(
+    'tasks',
+    broker=f'redis://{redis.host}:{redis.port}/{redis.db}',
+    backend=f'redis://{redis.host}:{redis.port}/{redis.db}',
+    include=['src.tasks.tasks']
+)
+
+celery_app.conf.update(
+    task_serializer='json',
+    accept_content=['json'],
+    result_serializer='json',
+    timezone='UTC',
+    enable_utc=True,
+)
